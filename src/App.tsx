959f58a3923fe2d986f43c2712fdb384f64fea9b
hrsk <hrsk.yegor@gmail.com>
2024-03-30 17:54:58 +0300

    align center

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import styled from 'styled-components';
 import { Card } from './Card';
 
 const AppWrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 
 `
 
